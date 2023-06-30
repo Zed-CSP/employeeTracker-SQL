48bd315536608a6235324c69a58534117824680c
Christopher Peret <cperet89@gmail.com>
2023-06-30 11:58:21 -0700

    'init()' prompt rough draft

--- server.js
+++ server.js
@@ -18,18 +18,93 @@ const connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect((err) => {
+db.connect((err) => {
     if (err) throw err;
 
     titleArt();
     // run the start function after the connection is made to prompt the user
     console.log('connected as id ' + connection.threadId);
 
-    start();
-}
-);
-
-
+    init();
+});
 
 // function which prompts the user for what action they should take
-const start = () => {};
+const init = () => {
+    inquirer.prompt(
+        {
+            type: 'list',
+            name: 'admin_action',
+            message: '?',
+            choices: [
+                'List all Departments',
+                'View Department Budgets',
+                'List all Roles',
+                'List all Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update an Employee Role',
+                'Update an Employee Manager',
+                'Delete a Department',
+                'Delete a Role',
+                'Delete an Employee',
+                'Exit',
+            ]
+        }
+    )
+    .then ((answer) => {
+        const { action } = answer
+
+        if (action === 'List all Departments') {
+            listAllDepartments();
+        }
+
+        if (action === 'View Department Budgets') {
+            viewDepartmentBudgets();
+        }
+
+        if (action === 'List all Roles') {
+            listAllRoles();
+        }
+
+        if (action === 'List all Employees') {
+            listAllEmployees();
+        }
+
+        if (action === 'Add a Department') {
+            addDepartment();
+        }
+
+        if (action === 'Add a Role') {
+            addRole();
+        }
+
+        if (action === 'Add an Employee') {
+            addEmployee();
+        }
+
+        if (action === 'Update an Employee Role') {
+            updateEmployeeRole();
+        }
+
+        if (action === 'Update an Employee Manager') {
+            updateEmployeeManager();
+        }
+
+        if (action === 'Delete a Department') {
+            deleteDepartment();
+        }
+
+        if (action === 'Delete a Role') {
+            deleteRole();
+        }
+
+        if (action === 'Delete an Employee') {
+            deleteEmployee();
+        }
+
+        if (action === 'Exit') {
+            exit();
+        }
+    })
+};
